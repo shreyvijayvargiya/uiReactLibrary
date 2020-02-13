05047af8a5aea838c277add6e99307ba20db18d8
shrey vijayvargiya <shreyvijayargiya>
2020-02-13 23:48:53 +0530

    done...

--- playground/Trick.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('CSSTricks', module).add('Trick',() => <p>Welcome</p>);
--- playground/modal.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { DemoModal } from './ui';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('UI', module).add('Modal', () => <DemoModal />);
